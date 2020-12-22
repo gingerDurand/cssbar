f598c58b34355a58dfc7eaa3f5413dd7c5ba5e50
ValerieGinger <66699387+ValerieGinger@users.noreply.github.com>
2020-12-22 16:39:16 -0500

    Added bootstrap attributes

--- public_html/bootstrap/wordpress/index.php
+++ public_html/bootstrap/wordpress/index.php
@@ -2,93 +2,185 @@
 <html>
 <head>
 	<title>Alves Example WP Theme</title>
+	<link rel="stylesheet" type="text/css" href="../bootstrap.css" />
 </head>
 <body>
-<header>
-	<h1>Alves</h1>
-	<p>1 Example Street, Anytown, 10100 USA</p>
-</header>
-<nav>
-	<ul>
-		<li><a href="">Home</a></li>
-		<li><a href="">About</a></li>
-		<li><a href="">Causes</a></li>
-		<li><a href="">Donate</a></li>
-		<li><a href="">Contact</a></li>
-	</ul>
-	<ul>
-		<li><a href="">Facebook</a></li>
-		<li><a href="">Twitter</a></li>
-		<li><a href="">Instagram</a></li>
-	</ul>
-</nav>
-<main>
-	<section>
-		<h2>Raise Your Helping Hand</h2>
-		<p>We are a non-profit organization, we are looking forward to a peaceful world by helping each other to join hands together to bring a better future for all children.</p>
-		<button type="button">Donate</button>
-		<button type="button">Contact</button>
-	</section>
+	<div class="container">
+		<div class="navbar navbar-light">
+			<header>
+				<h1><a href="https://wordpress.com/theme/alves" class="navbar-brand">Alves</a></h1>
+				<p>1 Example Street, Anytown, 10100 USA</p>
+			</header>
 
-	<section>
-		<h2>About Us</h2>
-		<h3>Our Beliefs</h3>
-		<p>We believe in a world where every child can read. Our mission is to invest in early childhood education in order to empower the next generation. We do that, by creating educational programs and providing necessary resources in underprivileged areas. We believe in smart fund allocation, and therefore we employ minimal staff.</p>
-	</section>
+			<nav class="navbar-expand-lg">
+				<ul class="navbar-nav justify-content-end">
+					<li class="nav-item">
+						<a href="" class="nav-link">Home</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">About</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">Causes</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">Donate</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">Contact</a>
+					</li>
+				</ul>
 
-	<section>
-		<h2>How You Can Help</h2>
-		<div>
-			<h3>Send Donation</h3>
-			<p>Giving online has never been more secure, convenient or hassle-free with our one-click donation. We also do accept standard cash and check donations at all of our locations.</p>
+				<ul class="navbar-nav justify-content-end">
+					<li class="nav-item">
+						<a href="" class="nav-link">Facebook</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">Twitter</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">Instagram</a>
+					</li>
+				</ul>
+
+			</nav>
 		</div>
-		<div>
-			<h3>Become a Volunteer</h3>
-			<p>You can get involved today by becoming a Volunteer. Sign up and you will be joining a group of change-makers, a network strong enough to impact positive change in the lives of children.</p>
-		</div>
-		<div>
-			<h3>Give Scholarship</h3>
-			<p>Your gift will help equip children in need with necessary resources, training and education while offering the promise of a brighter future. You can make a difference today by signing up.</p>
-		</div>
-	</section>
+	</div>
 
-	<section>
-		<h2>Testimonials</h2>
-		<h3>What People Say</h3>
-		<blockquote>
-			<p>The generosity of this organization makes it possible for me to continue having education opportunities in this difficult area.</p>
-			<cite>Jane Doe</cite>
-		</blockquote>
-		<blockquote>
-			<p>Wonderful job with those kids that need us and people from various places all over the world. I will definitely join your group as a volunteer.</p>
-			<cite>John Doe</cite>
-		</blockquote>
-	</section>
 
-	<section>
-		<h2>News</h2>
-		<h3>Recent Causes</h3>
+	<main>
+		<div class="container-full">
+			<section class="jumbotron text-center">
+				<div class="container">
+					<h2>Raise Your Helping Hand</h2>
+					<p>We are a non-profit organization, we are looking forward to a peaceful world by helping each other to join hands together to bring a better future for all children.</p>
+					<button type="button">Donate</button>
+					<button type="button">Contact</button>
+				</div>
+			</section>
 
-		<article>
-			<h2>Course: Teach Kids To Read</h2>
-			<p>August 22. 2020</p>
-			<p>We have developed a teacher training curriculum and were able to host training sessions for teachers and educational personnel about new techniques of teaching how to read. We believe this will empower teachers in classrooms to teach more effectively.</p>
-		</article>
-		<article>
-			<h2>Schoole Bus Initiative</h2>
-			<p>August 22. 2020</p>
-			<p>The focus of our last quarter has been working with local groups to improve bus networks.</p>
-		</article>
-		<article>
-			<h2>10,000 Pencils Deliveres to Schools</h2>
-			<p>August 22. 2020</p>
-			<p>This summer, because of the effort of our volunteers and the generous donations from our sponsors, we were able to deliver 10,000 pencils to children in need. We were able to reach the most unprivileged areas and provide local schools with this vital tool for education.</p>
-		</article>
-	</section>
+			<section class="jumbotron">
+				<div class="container">
+					<div class="row">
+						<div class="col-3">
+							<h2 class="h6">About Us</h2>
+							<h3>Our Beliefs</h3>
+						</div>
+						<div class="col-9">
+							<p>We believe in a world where every child can read. Our mission is to invest in early childhood education in order to empower the next generation. We do that, by creating educational programs and providing necessary resources in underprivileged areas. We believe in smart fund allocation, and therefore we employ minimal staff.</p>
+						</div>
+					</div>
+				</div>
+			</section>
 
-	<section>
-		
-	</section>
-</main>
-</body>
-</html>
+			<section class="jumbotron">
+				<div class="container">
+					<h2 class="text-center">How You Can Help</h2>
+					<div class="row">
+						<div class="col-4">
+							<h3>Send Donation</h3>
+							<p>Giving online has never been more secure, convenient or hassle-free with our one-click donation. We also do accept standard cash and check donations at all of our locations.</p>
+						</div>
+						<div class="col-4">
+							<h3>Become a Volunteer</h3>
+							<p>You can get involved today by becoming a Volunteer. Sign up and you will be joining a group of change-makers, a network strong enough to impact positive change in the lives of children.</p>
+						</div>
+						<div class="col-4">
+							<h3>Give Scholarship</h3>
+							<p>Your gift will help equip children in need with necessary resources, training and education while offering the promise of a brighter future. You can make a difference today by signing up.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="jumbotron">
+				<div class="container">
+					<h2 class="h6 text-center">Testimonials</h2>
+					<h3 class="text-center">What People Say</h3>
+					<div class="row">
+						<div class="col-6">
+							<blockquote>
+								<p>The generosity of this organization makes it possible for me to continue having education opportunities in this difficult area.</p>
+								<cite>Jane Doe</cite>
+							</blockquote>
+						</div>
+						<div class="col-6">
+							<blockquote>
+								<p>Wonderful job with those kids that need us and people from various places all over the world. I will definitely join your group as a volunteer.</p>
+								<cite>John Doe</cite>
+							</blockquote>
+						</div>
+					</div>
+				</section>
+
+				<section class="jumbotron">
+					<div class="container">
+						<h2 class="h6 text-center">News</h2>
+						<h3 class="text-center">Recent Causes</h3>
+
+						<div class="row">
+							<div class="col-4">
+								<article>
+									<h2>Course: Teach Kids To Read</h2>
+									<p>August 22. 2020</p>
+									<p>We have developed a teacher training curriculum and were able to host training sessions for teachers and educational personnel about new techniques of teaching how to read. We believe this will empower teachers in classrooms to teach more effectively.</p>
+								</article>
+							</div>
+							<div class="col-4">
+								<article>
+									<h2>Schoole Bus Initiative</h2>
+									<p>August 22. 2020</p>
+									<p>The focus of our last quarter has been working with local groups to improve bus networks.</p>
+								</article>
+							</div>
+							<div class="col-4">
+								<article>
+									<h2>10,000 Pencils Deliveres to Schools</h2>
+									<p>August 22. 2020</p>
+									<p>This summer, because of the effort of our volunteers and the generous donations from our sponsors, we were able to deliver 10,000 pencils to children in need. We were able to reach the most unprivileged areas and provide local schools with this vital tool for education.</p>
+								</article>
+							</div>
+						</div>
+					</section>
+
+					<section class="jumbotron text-center">
+						<div class="container">
+							<h2>Become a Volunteer</h2>
+							<p>With the aim of helping as many people as possible, we always lack enthusiastic volunteers. Please contact us for more info.</p>
+							<button type="button">Contact</button>
+						</div>
+					</section>
+				</div>
+			</main>
+
+			<footer>
+				<div class="container">
+					<div class="nav navbar navbar-light">
+						<div class="navbar-expand-lg">
+							<ul class="navbar-nav">
+								<li class="nav-item">
+									<a href="" class="nav-link">Home</a>
+								</li>
+								<li class="nav-item">
+									<a href="" class="nav-link">About</a>
+								</li>
+								<li class="nav-item">
+									<a href="" class="nav-link">Causes</a>
+								</li>
+								<li class="nav-item">
+									<a href="" class="nav-link">Donate</a>
+								</li>
+								<li class="nav-item">
+									<a href="" class="nav-link">Contact</a>
+								</li>
+							</ul>
+						</div>
+						<p class="justify-content-end"><strong>Alves</strong>, 
+							<a href="">Create a free website or blog at WordPress.com.</a>
+						</p>
+					</div>
+				</div>
+			</footer>
+
+		</body>
+		</html>
