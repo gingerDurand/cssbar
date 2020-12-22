997d1787808986c85e3e63eeb66a3de62ec6e8ee
ValerieGinger <66699387+ValerieGinger@users.noreply.github.com>
2020-12-22 15:20:18 -0500

    HTML for WP example theme

---
+++ public_html/bootstrap/wordpress/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Alves Example WP Theme</title>
+</head>
+<body>
+<header>
+	<h1>Alves</h1>
+	<p>1 Example Street, Anytown, 10100 USA</p>
+</header>
+<nav>
+	<ul>
+		<li><a href="">Home</a></li>
+		<li><a href="">About</a></li>
+		<li><a href="">Causes</a></li>
+		<li><a href="">Donate</a></li>
+		<li><a href="">Contact</a></li>
+	</ul>
+	<ul>
+		<li><a href="">Facebook</a></li>
+		<li><a href="">Twitter</a></li>
+		<li><a href="">Instagram</a></li>
+	</ul>
+</nav>
+<main>
+	<section>
+		<h2>Raise Your Helping Hand</h2>
+		<p>We are a non-profit organization, we are looking forward to a peaceful world by helping each other to join hands together to bring a better future for all children.</p>
+		<button type="button">Donate</button>
+		<button type="button">Contact</button>
+	</section>
+
+	<section>
+		<h2>About Us</h2>
+		<h3>Our Beliefs</h3>
+		<p>We believe in a world where every child can read. Our mission is to invest in early childhood education in order to empower the next generation. We do that, by creating educational programs and providing necessary resources in underprivileged areas. We believe in smart fund allocation, and therefore we employ minimal staff.</p>
+	</section>
+
+	<section>
+		<h2>How You Can Help</h2>
+		<div>
+			<h3>Send Donation</h3>
+			<p>Giving online has never been more secure, convenient or hassle-free with our one-click donation. We also do accept standard cash and check donations at all of our locations.</p>
+		</div>
+		<div>
+			<h3>Become a Volunteer</h3>
+			<p>You can get involved today by becoming a Volunteer. Sign up and you will be joining a group of change-makers, a network strong enough to impact positive change in the lives of children.</p>
+		</div>
+		<div>
+			<h3>Give Scholarship</h3>
+			<p>Your gift will help equip children in need with necessary resources, training and education while offering the promise of a brighter future. You can make a difference today by signing up.</p>
+		</div>
+	</section>
+
+	<section>
+		<h2>Testimonials</h2>
+		<h3>What People Say</h3>
+		<blockquote>
+			<p>The generosity of this organization makes it possible for me to continue having education opportunities in this difficult area.</p>
+			<cite>Jane Doe</cite>
+		</blockquote>
+		<blockquote>
+			<p>Wonderful job with those kids that need us and people from various places all over the world. I will definitely join your group as a volunteer.</p>
+			<cite>John Doe</cite>
+		</blockquote>
+	</section>
+
+	<section>
+		<h2>News</h2>
+		<h3>Recent Causes</h3>
+
+		<article>
+			<h2>Course: Teach Kids To Read</h2>
+			<p>August 22. 2020</p>
+			<p>We have developed a teacher training curriculum and were able to host training sessions for teachers and educational personnel about new techniques of teaching how to read. We believe this will empower teachers in classrooms to teach more effectively.</p>
+		</article>
+		<article>
+			<h2>Schoole Bus Initiative</h2>
+			<p>August 22. 2020</p>
+			<p>The focus of our last quarter has been working with local groups to improve bus networks.</p>
+		</article>
+		<article>
+			<h2>10,000 Pencils Deliveres to Schools</h2>
+			<p>August 22. 2020</p>
+			<p>This summer, because of the effort of our volunteers and the generous donations from our sponsors, we were able to deliver 10,000 pencils to children in need. We were able to reach the most unprivileged areas and provide local schools with this vital tool for education.</p>
+		</article>
+	</section>
+
+	<section>
+		
+	</section>
+</main>
+</body>
+</html>
