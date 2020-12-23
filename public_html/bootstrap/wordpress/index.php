d0de27ec309429ed36dd5d90492127e448950924
ValerieGinger <66699387+ValerieGinger@users.noreply.github.com>
2020-12-23 10:58:12 -0500

    Add Album

--- public_html/bootstrap/wordpress/index.php
+++ public_html/bootstrap/wordpress/index.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 	<title>Alves Example WP Theme</title>
-	<link rel="stylesheet" type="text/css" href="../bootstrap.css" />
+	<link rel="stylesheet" href="../bootstrap.css" />
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 	<div class="container">
@@ -49,63 +50,63 @@
 
 
 	<main>
-		<div class="container-full">
-			<section class="jumbotron text-center">
+		<div class="container-full text-light">
+			<section class="jumbotron bg-01 text-center">
 				<div class="container">
-					<h2>Raise Your Helping Hand</h2>
+					<h2 class="pb-3 display-4 font-weight-light">Raise Your Helping Hand</h2>
 					<p>We are a non-profit organization, we are looking forward to a peaceful world by helping each other to join hands together to bring a better future for all children.</p>
 					<button type="button">Donate</button>
 					<button type="button">Contact</button>
 				</div>
 			</section>
 
-			<section class="jumbotron">
+			<section class="jumbotron bg-02">
 				<div class="container">
 					<div class="row">
-						<div class="col-3">
-							<h2 class="h6">About Us</h2>
-							<h3>Our Beliefs</h3>
+						<div class="col-4">
+							<h2 class="h6 font-weight-light">About Us</h2>
+							<h3 class="h1 font-weight-light">Our Beliefs</h3>
 						</div>
-						<div class="col-9">
+						<div class="col-8">
 							<p>We believe in a world where every child can read. Our mission is to invest in early childhood education in order to empower the next generation. We do that, by creating educational programs and providing necessary resources in underprivileged areas. We believe in smart fund allocation, and therefore we employ minimal staff.</p>
 						</div>
 					</div>
 				</div>
 			</section>
 
-			<section class="jumbotron">
+			<section class="jumbotron bg-03">
 				<div class="container">
-					<h2 class="text-center">How You Can Help</h2>
+					<h2 class="h1 pb-5 text-center font-weight-light">How You Can Help</h2>
 					<div class="row">
 						<div class="col-4">
-							<h3>Send Donation</h3>
+							<h3 class="pb-3 font-weight-light">Send Donation</h3>
 							<p>Giving online has never been more secure, convenient or hassle-free with our one-click donation. We also do accept standard cash and check donations at all of our locations.</p>
 						</div>
 						<div class="col-4">
-							<h3>Become a Volunteer</h3>
+							<h3 class="pb-3 font-weight-light">Become a Volunteer</h3>
 							<p>You can get involved today by becoming a Volunteer. Sign up and you will be joining a group of change-makers, a network strong enough to impact positive change in the lives of children.</p>
 						</div>
 						<div class="col-4">
-							<h3>Give Scholarship</h3>
+							<h3 class="pb-3 font-weight-light">Give Scholarship</h3>
 							<p>Your gift will help equip children in need with necessary resources, training and education while offering the promise of a brighter future. You can make a difference today by signing up.</p>
 						</div>
 					</div>
 				</div>
 			</section>
 
-			<section class="jumbotron">
+			<section class="jumbotron bg-04">
 				<div class="container">
-					<h2 class="h6 text-center">Testimonials</h2>
-					<h3 class="text-center">What People Say</h3>
-					<div class="row">
+					<h2 class="h6 pb-3 text-center font-weight-light">Testimonials</h2>
+					<h3 class="h1 pb-5 text-center font-weight-light">What People Say</h3>
+					<div class="row pt-5">
 						<div class="col-6">
-							<blockquote>
+							<blockquote class="blockquote">
 								<p>The generosity of this organization makes it possible for me to continue having education opportunities in this difficult area.</p>
 								<cite>Jane Doe</cite>
 							</blockquote>
 						</div>
 						<div class="col-6">
-							<blockquote>
+							<blockquote class="blockquote">
 								<p>Wonderful job with those kids that need us and people from various places all over the world. I will definitely join your group as a volunteer.</p>
 								<cite>John Doe</cite>
 							</blockquote>
@@ -113,39 +114,39 @@
 					</div>
 				</section>
 
-				<section class="jumbotron">
+				<section class="jumbotron bg-05">
 					<div class="container">
-						<h2 class="h6 text-center">News</h2>
-						<h3 class="text-center">Recent Causes</h3>
+						<h2 class="h6 pb-3 text-center font-weight-light">News</h2>
+						<h3 class="h1 pb-5 text-center font-weight-light">Recent Causes</h3>
 
-						<div class="row">
-							<div class="col-4">
+						<div class="row pt-5 justify-content-between">
+							<div class="col-3">
 								<article>
-									<h2>Course: Teach Kids To Read</h2>
-									<p>August 22. 2020</p>
+									<h3 class="pb-2 font-weight-light">Course: Teach Kids To Read</h3>
+									<p><small>August 22. 2020</small></p>
 									<p>We have developed a teacher training curriculum and were able to host training sessions for teachers and educational personnel about new techniques of teaching how to read. We believe this will empower teachers in classrooms to teach more effectively.</p>
 								</article>
 							</div>
-							<div class="col-4">
+							<div class="col-3">
 								<article>
-									<h2>Schoole Bus Initiative</h2>
-									<p>August 22. 2020</p>
+									<h3 class="pb-2 font-weight-light">Schoole Bus Initiative</h3>
+									<p><small>August 22. 2020</small></p>
 									<p>The focus of our last quarter has been working with local groups to improve bus networks.</p>
 								</article>
 							</div>
-							<div class="col-4">
+							<div class="col-3">
 								<article>
-									<h2>10,000 Pencils Deliveres to Schools</h2>
-									<p>August 22. 2020</p>
+									<h3 class="pb-2 font-weight-light">10,000 Pencils Deliveres to Schools</h3>
+									<p><small>August 22. 2020</small></p>
 									<p>This summer, because of the effort of our volunteers and the generous donations from our sponsors, we were able to deliver 10,000 pencils to children in need. We were able to reach the most unprivileged areas and provide local schools with this vital tool for education.</p>
 								</article>
 							</div>
 						</div>
 					</section>
 
-					<section class="jumbotron text-center">
+					<section class="jumbotron bg-06 text-center">
 						<div class="container">
-							<h2>Become a Volunteer</h2>
+							<h2 class="h1 pb-5 font-weight-light">Become a Volunteer</h2>
 							<p>With the aim of helping as many people as possible, we always lack enthusiastic volunteers. Please contact us for more info.</p>
 							<button type="button">Contact</button>
 						</div>
